ff1d517a13aceb29c7a2e9d58c420946b7c0e5fe
raimatt <122494842+raimatt@users.noreply.github.com>
2024-08-12 08:44:09 -1000

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:5000/get_recipes'
-data = {"ingredients": ["chicken", "tomato", "cheese"]}
-
-response = requests.post(url, json=data)
-print(response.json())
